70ba6bd33c53d99085ff932ef137f975a9743c8d
James McMahon <j.mcmahon1@nhs.net>
2020-04-28 14:47:18 +0100

    Add line to exclude people who travelled

--- code/plot outputs.R
+++ code/plot outputs.R
@@ -16,14 +16,17 @@ scot_data <- scot_data %>%
     )
   )
 
-## Breakdown of gender, age and death 
-pop_data <- scot_data %>% 
-  group_by(subjid) %>% 
-  summarise(age = first(na.omit(age.factor)),
-            sex = first(na.omit(sex)),
-            ethnicity = first(na.omit(ethnicity)),
-            outcome = first(na.omit(dsterm))) %>% 
-  mutate(died = case_when(outcome == "Death" ~ "Yes", TRUE ~ "No") %>% factor(levels = c("No", "Yes"))) 
+## Breakdown of gender, age and death
+pop_data <- scot_data %>%
+  filter(!(subjid %in% travelled)) %>%
+  group_by(subjid) %>%
+  summarise(
+    age = first(na.omit(age.factor)),
+    sex = first(na.omit(sex)),
+    ethnicity = first(na.omit(ethnicity)),
+    outcome = first(na.omit(dsterm))
+  ) %>%
+  mutate(died = case_when(outcome == "Death" ~ "Yes", TRUE ~ "No") %>% factor(levels = c("No", "Yes")))
 
 
 # Population pyramid
@@ -34,6 +37,3 @@ ggplot(pop_data, aes(x = age, fill = sex, alpha = died)) +
   scale_alpha_ordinal("Died", range = c(0.3, 1)) +
   coord_flip() +
   theme_minimal()
-
-
-

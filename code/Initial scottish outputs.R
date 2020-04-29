54a7b20587c1796f22c5093f58da5fdf956a3e69
James McMahon <j.mcmahon1@nhs.net>
2020-04-29 12:38:38 +0100

    Add plot code

--- code/Initial scottish outputs.R
+++ code/Initial scottish outputs.R
@@ -171,4 +171,22 @@ admission_data <- topline %>%
 
 
 
+# Plots -------------------------------------------------------------------
 
+# Population pyramid
+ggplot(pop_data, aes(x = age, fill = sex, alpha = died)) +
+  geom_bar(data = filter(pop_data, sex == "Female")) +
+  geom_bar(data = filter(pop_data, sex == "Male"), aes(y = ..count.. * (-1))) +
+  scale_fill_discrete("Sex at birth") +
+  scale_alpha_ordinal("Died", range = c(0.3, 1)) +
+  coord_flip() +
+  theme_minimal() +
+  theme(legend.position = "bottom") +
+  ylim(-100, 100) +
+  xlab("Age") +
+  ylab("Count of subjects") +
+  ggtitle("Population pyramid of ISARIC subjects from Scottish hospitals",
+    subtitle = str_glue("Excludes {n_travelled} who travelled in the 14 days prior to admission",
+      n_travelled = length(travelled)
+    )
+  )

be785d10b78da5fb00b7a8d705ed1c23a71adbe1
James McMahon <j.mcmahon1@nhs.net>
2020-04-29 08:53:50 +0100

    Remove extra files Lookup code now in main extract code

--- code/00 - Get Scottish data.R
+++ code/00 - Get Scottish data.R
@@ -58,8 +58,24 @@ if (class(extract) == "character") {
 }
 
 ## Add on Location details for Scottish hospitals where we can
-# Lookup created by create_scottish_lookups.R
-scot_locations <- read_csv("lookups/scot_locations.csv")
+# Get Hosptial Lookup and Board names from the NHS Scotland Opendata platform
+
+# Hospital codes
+# https://www.opendata.nhs.scot/dataset/hospital-codes/resource/c698f450-eeed-41a0-88f7-c1e40a568acc
+scot_locations <- read_csv("https://www.opendata.nhs.scot/dataset/cbd1802e-0e04-4282-88eb-d7bdcfb120f0/resource/c698f450-eeed-41a0-88f7-c1e40a568acc/download/current_nhs_hospitals_in_scotland_200420.csv",
+                           col_types = c("ccc-c----"))
+
+# Board names (using 2019 specification)
+# https://www.opendata.nhs.scot/dataset/geography-codes-and-labels/resource/f177be64-e94c-4ddf-a2ee-ea58d648d55a
+hb_names <- read_csv("https://www.opendata.nhs.scot/dataset/9f942fdb-e59e-44f5-b534-d6e17229cc7b/resource/f177be64-e94c-4ddf-a2ee-ea58d648d55a/download/hb2019_codes_and_labels_21042020.csv",
+                     col_types = "cc-")
+
+# Join on HB names
+scot_locations <- left_join(scot_locations, hb_names, by = "HB") %>% 
+  # Do some renaming to make it match old lookup so we don't have to change other code
+  rename(Locname = LocationName,
+         HB2019Name = HBName,
+         HB2019 = HB)
 
 extract <- extract %>%
   mutate(hospid = str_sub(subjid, end = 5)) %>%
@@ -68,8 +84,7 @@ extract <- extract %>%
 ## Work out what the Scottish data access groups are
 scot_groups <- extract %>%
   filter(!is.na(Locname)) %>%
-  distinct(redcap_data_access_group) %>%
-  write_csv("lookups/scottish_data_access_groups.csv")
+  distinct(redcap_data_access_group)
 
 # Filter to Scottish data
 scot_data <- extract %>%
@@ -88,4 +103,4 @@ source("CCPUKSARI_R_2020-03-04_1532.R")
 scot_data <- data
 
 # Clean up
-rm(extract, data, tries)
+rm(extract, data, tries, hb_names)

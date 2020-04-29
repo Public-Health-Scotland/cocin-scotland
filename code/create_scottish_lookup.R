b3559dfb7b299ac9d46f02029204af93fa98e7f1
James McMahon <j.mcmahon1@nhs.net>
2020-04-29 08:48:17 +0100

    Rewrite to pull files from Opendata

--- code/create_scottish_lookup.R
+++ code/create_scottish_lookup.R
@@ -1,15 +1,18 @@
-# Create lookup for Scottish hospitals
-# Lookup includes 
-# hospid (Location code)
-# Locname (Name of the hospital)
-# Postcode
-# HB2019 and HB2019Name (the code and name of the HB for the hospital)
+# Get Hosptial Lookup and Board names from the NHS Scotland Opendata platform
 
-scot_locations <- read_rds("//stats/cl-out/lookups/Unicode/National Reference Files/location.rds") %>%
-  select(Location, Locname, Postcode)
-hb_postcodes <- read_rds("//stats/cl-out/lookups/Unicode/Geography/Scottish Postcode Directory/Scottish_Postcode_Directory_2019_2.rds") %>%
-  select(pc8, HB2019, HB2019Name)
+# Hospital codes
+# https://www.opendata.nhs.scot/dataset/hospital-codes/resource/c698f450-eeed-41a0-88f7-c1e40a568acc
+scot_locations <- read_csv("https://www.opendata.nhs.scot/dataset/cbd1802e-0e04-4282-88eb-d7bdcfb120f0/resource/c698f450-eeed-41a0-88f7-c1e40a568acc/download/current_nhs_hospitals_in_scotland_200420.csv",
+                           col_types = c("ccc-c----"))
 
-scot_locations <- left_join(scot_locations, hb_postcodes, by = c("Postcode" = "pc8"))
+# Board names (using 2019 specification)
+# https://www.opendata.nhs.scot/dataset/geography-codes-and-labels/resource/f177be64-e94c-4ddf-a2ee-ea58d648d55a
+hb_names <- read_csv("https://www.opendata.nhs.scot/dataset/9f942fdb-e59e-44f5-b534-d6e17229cc7b/resource/f177be64-e94c-4ddf-a2ee-ea58d648d55a/download/hb2019_codes_and_labels_21042020.csv",
+                     col_types = "cc-")
 
-write_csv(scot_locations, "lookups/scot_locations.csv")
+# Join on HB names
+scot_locations <- left_join(scot_locations, hb_names, by = "HB") %>% 
+# Do some renaming to make it match old lookup so we don't have to change other code
+  rename(Locname = LocationName,
+         HB2019Name = HBName,
+         HB2019 = HB)

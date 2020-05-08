c0912c9ec6a1ecd5c6535e57bbb2bdf1bb5a3383
alicebyers5 <alice.byers@nhs.net>
2020-05-08 17:03:53 +0100

    Tidy open data extract

--- extract-data/01_get-scottish-data.R
+++ extract-data/01_get-scottish-data.R
@@ -3,15 +3,16 @@
 ## ICU and Hospital Work Stream 
 ## Analytical Team 
 
-## Pull data
 
-# Libraries
+### 0 - Load packages ----
+
 library(RCurl)
 library(tidyverse)
 library(lubridate)
 library(finalfit)
 library(tidylog)
 library(Hmisc)
+library(janitor)
 
 
 ### 1 - Extract data from RedCap via API ----
@@ -65,18 +66,27 @@ if (class(extract) == "character") {
 
 ### 2 - Select Scottish data
 
-## Add on Location details for Scottish hospitals where we can
-# Get Hosptial Lookup and Board names from the NHS Scotland Opendata platform
+scot_locations <-
+  
+  # Extract Scottish hospital location codes
+  read_csv(
+    paste0("https://www.opendata.nhs.scot/dataset/cbd1802e-0e04-4282-88eb-",
+           "d7bdcfb120f0/resource/c698f450-eeed-41a0-88f7-c1e40a568acc/",
+           "download/current_nhs_hospitals_in_scotland_200420.csv")
+  ) %>%
+  
+  # Extract health board names and join
+  left_join(
+    read_csv(
+      paste0("https://www.opendata.nhs.scot/dataset/9f942fdb-e59e-44f5-",
+             "b534-d6e17229cc7b/resource/f177be64-e94c-4ddf-a2ee-ea58d648d55a/",
+             "download/hb2019_codes_and_labels_21042020.csv")
+    ),
+    by = "HB"
+  ) %>%
+  
+  clean_names()
 
-# Hospital codes
-# https://www.opendata.nhs.scot/dataset/hospital-codes/resource/c698f450-eeed-41a0-88f7-c1e40a568acc
-scot_locations <- read_csv("https://www.opendata.nhs.scot/dataset/cbd1802e-0e04-4282-88eb-d7bdcfb120f0/resource/c698f450-eeed-41a0-88f7-c1e40a568acc/download/current_nhs_hospitals_in_scotland_200420.csv",
-                           col_types = c("ccc-c----"))
-
-# Board names (using 2019 specification)
-# https://www.opendata.nhs.scot/dataset/geography-codes-and-labels/resource/f177be64-e94c-4ddf-a2ee-ea58d648d55a
-hb_names <- read_csv("https://www.opendata.nhs.scot/dataset/9f942fdb-e59e-44f5-b534-d6e17229cc7b/resource/f177be64-e94c-4ddf-a2ee-ea58d648d55a/download/hb2019_codes_and_labels_21042020.csv",
-                     col_types = "cc-")
 
 # Join on HB names
 scot_locations <- left_join(scot_locations, hb_names, by = "HB") %>% 

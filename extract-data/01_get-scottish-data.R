a2b0fbf7e5ac0e549dfed2f2909fa5ff57e1c23c
James McMahon <j.mcmahon1@nhs.net>
2020-05-26 10:51:38 +0100

    Only read necessary columns
    Reads quicker when we've specified types

--- extract-data/01_get-scottish-data.R
+++ extract-data/01_get-scottish-data.R
@@ -64,7 +64,12 @@ scot_locations <-
   read_csv(
     paste0("https://www.opendata.nhs.scot/dataset/cbd1802e-0e04-4282-88eb-",
            "d7bdcfb120f0/resource/c698f450-eeed-41a0-88f7-c1e40a568acc/",
-           "download/current_nhs_hospitals_in_scotland_200420.csv")
+           "download/current_nhs_hospitals_in_scotland_200420.csv"),
+    col_types = cols_only(
+      Location = col_character(),
+      LocationName = col_character(),
+      HB = col_character()
+    )
   ) %>%
   
   # Extract health board names and join
@@ -72,7 +77,10 @@ scot_locations <-
     read_csv(
       paste0("https://www.opendata.nhs.scot/dataset/9f942fdb-e59e-44f5-",
              "b534-d6e17229cc7b/resource/f177be64-e94c-4ddf-a2ee-ea58d648d55a/",
-             "download/hb2019_codes_and_labels_21042020.csv")
+             "download/hb2019_codes_and_labels_21042020.csv"),
+      col_types = cols_only(
+        HB = col_character()
+      )
     ),
     by = "HB"
   ) %>%

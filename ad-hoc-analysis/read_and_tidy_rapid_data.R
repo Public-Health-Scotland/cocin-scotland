42545d4ab4dbe063e751c55ee99bfceef4781c49
James McMahon <j.mcmahon1@nhs.net>
2020-06-01 15:52:13 +0100

    Use general set-up and add code to get latest RAPID file

--- ad-hoc-analysis/read_and_tidy_rapid_data.R
+++ ad-hoc-analysis/read_and_tidy_rapid_data.R
@@ -1,9 +1,39 @@
-library(readr)
-library(dplyr)
-library(lubridate)
+source("extract-data/00_setup-environment.R")
 
 # Read data found here: 
 # \\stats\PHSCOVID19_Analysis\RAPID Reporting\Daily_extracts
+linked_file_path <- path("//stats", "PHSCOVID19_Analysis", "RAPID Reporting", "Daily_extracts", "rapid_ecoss_joined.rds")
+if (vpn_active()) {
+  message("Connection to NHS network is active")
+  if (file_exists(linked_file_path)) {
+    server_last_modified <- file_info(linked_file_path) %>%
+      pull(modification_time)
+
+    local_last_modified <- file_info("data/rapid_ecoss_joined.rds") %>%
+      pull(modification_time)
+
+    if (date(server_last_modified) > date(local_last_modified) | is.na(local_last_modified)) {
+      message(str_glue("The server file looks newer (modified:{server_date}), so replacing local file (modified:{local_date})",
+        server_date = date(server_last_modified),
+        local_date = date(local_last_modified)
+      ))
+      file_copy(linked_file_path, "data/rapid_ecoss_joined.rds", overwrite = TRUE)
+      
+      local_last_modified <- server_last_modified
+      
+      message("New file copied")
+    } else {
+      message(str_glue("Local file is current (modified:{local_date})",
+        local_date = date(local_last_modified)
+      ))
+    }
+  } else {
+    message("File 'rapid_ecoss_joined.rds' does not exist")
+  }
+} else {
+  message("Not connected to NHS network so can't check for new file")
+}
+
 rapid <- read_rds(here("data", "rapid_ecoss_joined.rds")) %>%
   select(
     chi_number,

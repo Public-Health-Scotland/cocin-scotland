bcf9b57e03a7441aece25137f8b26e14d8540d2f
James McMahon <j.mcmahon1@nhs.net>
2020-06-05 13:55:23 +0100

    tidy

--- ad-hoc-analysis/output_completeness_estimates.R
+++ ad-hoc-analysis/output_completeness_estimates.R
@@ -38,15 +38,15 @@ hosp_completeness %>%
     pct_complete
   ) %>%
   bind_rows(hosp_completeness %>%
-    summarise(
-      cocin_patients = sum(cocin_patients, na.rm = TRUE),
-      rapid_patients = sum(rapid_patients, na.rm = TRUE)
-    ) %>%
-    mutate(
-      Health_Board = "Scotland",
-      pct_complete = cocin_patients / rapid_patients * 100,
-      order = 1
-    )) %>%
+              summarise(
+                cocin_patients = sum(cocin_patients, na.rm = TRUE),
+                rapid_patients = sum(rapid_patients, na.rm = TRUE)
+              ) %>%
+              mutate(
+                Health_Board = "Scotland",
+                pct_complete = cocin_patients / rapid_patients * 100,
+                order = 1
+              )) %>%
   arrange(order, Health_Board, Hospital_Name) %>%
   select(-order) %>%
   write_csv(

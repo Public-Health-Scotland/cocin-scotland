4a63bd8390642404db820aef5f9b1451fc50ef9f
James McMahon <j.mcmahon1@nhs.net>
2020-06-05 17:20:33 +0100

    tidy

--- functions/fix_age_sex_from_chi.R
+++ functions/fix_age_sex_from_chi.R
@@ -22,18 +22,18 @@ fix_age_sex_from_chi <- function(data) {
     group_by(subjid) %>%
     summarise_all(~ first(na.omit(.))) %>%
     filter(phsmethods::chi_pad(nhs_chi) %>% phsmethods::chi_check() == "Valid CHI")
-
+  
   missing_age <- valid_chis %>% filter(is.na(age))
   missing_sex <- valid_chis %>% filter(is.na(sex))
-
+  
   fixed_age <- missing_age %>%
     filter(is.na(agedat)) %>%
     mutate(
       agedat = dob_from_chi(nhs_chi),
-
+      
       # Date at which to calculate age
       anydat = coalesce(hostdat, daily_dsstdat, cestdat, dsstdat),
-
+      
       # Calculate age using DOB and anydat derived above
       age = interval(agedat, anydat) %>%
         as.period() %>%
@@ -41,21 +41,21 @@ fix_age_sex_from_chi <- function(data) {
         as.double()
     ) %>%
     select(subjid, agedat, age)
-
+  
   fixed_sex <- missing_sex %>%
     mutate(sex = sex_from_chi(sex)) %>%
     select(subjid, sex)
-
+  
   fixed_data <- reduce(list(data, fixed_sex, fixed_age),
-    left_join,
-    by = "subjid",
-    suffix = c("", "_fix")
+                       left_join,
+                       by = "subjid",
+                       suffix = c("", "_fix")
   ) %>%
     mutate(
       age = if_else(is.na(age_fix), age, age_fix),
       agedat = if_else(is.na(agedat_fix), agedat, agedat_fix),
       sex = if_else(is.na(sex_fix), sex, sex_fix)
     )
-
+  
   return(fixed_data)
 }

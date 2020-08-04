6621853ac4e94732544fbe73b74fcaacaf372ca0
James McMahon <j.mcmahon1@nhs.net>
2020-08-04 17:03:10 +0100

    Update to allow missing files

--- functions/latest_extract_date.R
+++ functions/latest_extract_date.R
@@ -1,12 +1,14 @@
 # function to take latest extract from data folder
 
 latest_extract_date <- function() {
-  list.files(here("data"),
+  latest_date <- list.files(here("data"),
     pattern = "^\\d{4}-\\d{2}-\\d{2}_\\d{2}-\\d{2}_"
   ) %>%
-    str_sub(1, 16) %>%
-    max()
     if_else(stringr::str_detect(., regex("scot-data", ignore_case = TRUE)), ., NA_character_) %>%
+    stringr::str_sub(1, 16) %>%
+    max(na.rm = T)
+  
+  return(dplyr::if_else(is.na(latest_date), "1900-01-01_00-00", latest_date))
 }
 
 

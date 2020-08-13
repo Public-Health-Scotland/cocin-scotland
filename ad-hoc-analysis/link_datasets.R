4485384edf2eedcdf345985987df3ecc5ad3e25b
Mark Hamilton <markha06@phs.scot>
2020-08-13 11:49:51 +0100

    Added Linkage and IMOVE recoding files

---
+++ ad-hoc-analysis/link_datasets.R
@@ -0,0 +1,48 @@
+source("extract-data/00_setup-environment.R")
+source("linked_extracts/00_all_extracts.R")
+
+# Read in RAPID data
+
+# define date of RAPID extraction
+rapid_date <- file_info(path(here("data"),  "rapid_ecoss_joined.rds")) %>%
+  pull(modification_time) %>%
+  date()
+
+# read in RAPID data
+rapid_data <- read_rds(path(here("data", str_glue("{rapid_date}_RAPID-cleaned-filtered.rds"))))
+
+### Create Linkages
+
+# COCIN (also includes all RAPID data)
+source("ad-hoc-analysis/linkage_COCIN.R")
+
+# SICSAG
+source("ad-hoc-analysis/linkage_sicsag.R")
+
+# SMR01
+source("ad-hoc-analysis/linkage_smr01_rapid.R")
+
+# NRS
+source("ad-hoc-analysis/linkage_nrs.R")
+
+
+### Link on all datasets
+data <- rapid_cocin %>%
+  left_join(rapid_icu, by = c("chi_number","adm_date")) %>%
+  left_join(rapid_prevhosp, by = c("chi_number","adm_date")) %>%
+  left_join(rapid_deaths, by = c("chi_number","adm_date"))
+
+# write dataset
+write_rds(data, path(here("data", str_glue("Linked_Dataset.rds"))), 
+          compress = "gz")
+
+# remove datasets not required
+rm(rapid_data, rapid_cocin, rapid_deaths, rapid_icu, rapid_prevhosp, rapid_nrs)
+
+
+### IMOVE Recode
+source("ad-hoc-analysis/IMOVE_Recode.R")
+
+# write dataset
+write_rds(IMOVE_data, path(here("data", str_glue("IMOVE_data.rds"))), 
+          compress = "gz")
---
+++ ad-hoc-analysis/linkage_nrs.R
@@ -0,0 +1,30 @@
+source("extract-data/00_setup-environment.R")
+
+# Read in SMR01 extract
+NRS <- read_rds(path(here("data", str_glue("NRS_extract.rds"))))
+
+# Check whether patient in RAPID data has died and DOD
+deaths1 <- rapid_data %>%
+  inner_join(NRS, by = "chi_number") %>%
+  mutate(death = "Yes") %>%
+  select(chi_number, adm_date, death, date_of_death)
+
+# Check whether patient died of COVID or not
+deaths2 <- rapid_data %>%
+  inner_join(NRS, by = "chi_number") %>%
+  filter_at(vars(contains("cause_of_death")), any_vars(str_detect(., 'U07'))) %>%
+  mutate(covid_death = "Yes") %>%
+  select(chi_number, adm_date, covid_death)
+
+# Join together
+deaths <- deaths1 %>%
+  left_join(deaths2, by = c("chi_number","adm_date")) %>%
+  mutate(covid_death = ifelse(is.na(covid_death), "No", covid_death))
+
+# Join on to all data
+rapid_deaths <- rapid_data %>%
+  left_join(deaths, by = c("chi_number","adm_date")) %>%
+  mutate(date_of_death = as.Date(date_of_death)) %>%
+  select(chi_number, adm_date, death, date_of_death, covid_death)
+
+rm(NRS, deaths, deaths1, deaths2)

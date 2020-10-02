f0271dfc654683e854890b96752520ddc913119f
James McMahon <j.mcmahon1@nhs.net>
2020-10-02 15:47:10 +0100

    Rewrite to avoid bug with missing CHIs

--- ad-hoc-analysis/linkage_nrs.R
+++ ad-hoc-analysis/linkage_nrs.R
@@ -4,27 +4,22 @@ source("extract-data/00_setup-environment.R")
 NRS <- read_rds(path(here("data", str_glue("NRS_extract.rds"))))
 
 # Check whether patient in RAPID data has died and DOD
-deaths1 <- rapid_data %>%
-  inner_join(NRS, by = "chi_number") %>%
-  mutate(death = "Yes") %>%
-  select(chi_number, adm_date, death, date_of_death)
-
-# Check whether patient died of COVID or not
-deaths2 <- rapid_data %>%
-  inner_join(NRS, by = "chi_number") %>%
-  filter_at(vars(contains("cause_of_death")), any_vars(str_detect(., "U07"))) %>%
-  mutate(covid_death = "Yes") %>%
-  select(chi_number, adm_date, covid_death)
-
-# Join together
-deaths <- deaths1 %>%
-  left_join(deaths2, by = c("chi_number", "adm_date")) %>%
-  mutate(covid_death = if_else(is.na(covid_death), "No", covid_death))
+deaths <- NRS %>%
+  # Remove invalid (mostly missing) CHIs
+  filter(chi_check(chi_number) == "Valid CHI") %>%
+  # Filter deaths to only chis which appear in RAPID
+  semi_join(rapid_data, by = "chi_number") %>%
+  # Check whether patient died of COVID or not
+  left_join(filter_at(., vars(contains("cause_of_death")), any_vars(str_detect(., "U07"))) %>%
+    mutate(covid_death = "Yes") %>%
+    select(chi_number, covid_death)) %>%
+  mutate(death = "Yes")
 
 # Join on to all data
 rapid_deaths <- rapid_data %>%
-  left_join(deaths, by = c("chi_number", "adm_date")) %>%
+  left_join(deaths, by = c("chi_number")) %>%
   mutate(date_of_death = as_date(date_of_death)) %>%
-  select(chi_number, adm_date, death, date_of_death, covid_death)
+  select(chi_number, adm_date, death, date_of_death, covid_death) %>%
+  replace_na(list(death = "No", covid_death = "No"))
 
-rm(NRS, deaths, deaths1, deaths2)
+rm(NRS, deaths)

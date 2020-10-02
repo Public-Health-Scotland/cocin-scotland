337963a7ace4a90ee1a133a4b911bcf3628e5a10
James McMahon <j.mcmahon1@nhs.net>
2020-10-02 15:50:42 +0100

    Updates for 2 October submission

--- ad-hoc-analysis/pseudonymise_data.R
+++ ad-hoc-analysis/pseudonymise_data.R
@@ -1,40 +1,25 @@
 # Pseudonymise data
 
-IMOVE_data_anon <- anonymise_chi(IMOVE_data, hosp_id2, lookup_file = "CHI_lookup.rds")
-
-table(test$multiple_episode, exclude = NULL)
-table(test$multiple_hosp, exclude = NULL)
-table(rapid_data$readmission, exclude = NULL)
-
-test <- IMOVE_data %>%
-  filter(!is.na(onsetdate))
+IMOVE_data_anon <- anonymise_chi(
+  data = IMOVE_data,
+  chi = hosp_id2,
+  lookup_file = "CHI_lookup_salted.rds",
+  key = rstudioapi::askForPassword("Enter (secret) key for hashing  or press cancel to not use one")
+)
 
 # APPEND ANON ID WITH MARKER FOR NUMBER OF ADMISSION IN DATA
 IMOVE_data_anon <- IMOVE_data_anon %>%
-  # group by ID
-  group_by(anon_id) %>%
-  # Sort by admission and discharge dates
-  arrange(admitdate, dischargedate) %>%
-  # Get row number and max row number
-  mutate(row = row_number(),
-         max_row = max(row)) %>%
-  # Ungroup
-  ungroup() %>%
-  # If ID has more than one row in data, append the row number
-  mutate(id_append = ifelse(max_row > 1, as.character(row), ""),
-         anon_id = paste0(anon_id, id_append)) %>%
-  # remove variables not required
-  select(-row, -max_row, -id_append) %>%
+  mutate(anon_id = str_glue("{anon_id}-{multiple_episode}")) %>% 
   # order variables
   select(idcountry, anon_id, everything()) %>%
   # Remove Postcode variable
   select(-postcode)
 
 # Removing NA onsetdate
-IMOVE_data_anon <- IMOVE_data_anon %>%
-  filter(!is.na(onsetdate)) 
+# IMOVE_data_anon <- IMOVE_data_anon %>%
+#   filter(!is.na(onsetdate)) 
 
-write_rds(IMOVE_data_anon, "data/IMOVE_data_27-08-20_Anon.rds")
-write_csv(IMOVE_data_anon, "data/IMOVE_data_27-08-20_Anon.csv")
+write_rds(IMOVE_data_anon, here("data", str_glue("IMOVE_data_{today()}_Anon.rds")), compress = "gz")
+write_csv(IMOVE_data_anon, here("data", str_glue("IMOVE_data_{today()}_Anon.csv")))
 
-test <- read_rds("data/IMOVE_data_27-08-20_Anon.rds")
+test <- read_rds(here("data", str_glue("IMOVE_data_{today()}_Anon.rds")))

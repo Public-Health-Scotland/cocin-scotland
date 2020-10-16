bbe2062ab18d42cb08e192a1396f68b190f57bba
James McMahon <j.mcmahon1@nhs.net>
2020-10-16 10:39:22 +0100

    Included checks (from IMOVE checks branch)

--- linked_extracts/02_output_dataset_versions.R
+++ linked_extracts/02_output_dataset_versions.R
@@ -6,6 +6,11 @@ write_rds(IMOVE_data, path(here("data", "IMOVE_data.rds")),
           compress = "gz"
 )
 
+# Run and view checks
+# TODO finish checks
+rmarkdown::render("linked_extracts/check_linked_data.Rmd")
+rstudioapi::viewer("linked_extracts/check_linked_data.html")
+
 ### Genomics Recode
 source("linked_extracts/output_recode/Genomics_Recode.R")
 

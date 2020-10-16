ebc5c3adb9160e8f4111917b1897f7107ff818af
James McMahon <j.mcmahon1@nhs.net>
2020-10-16 08:40:10 +0100

    echo when sourcing

--- linked_extracts/00_all_extracts.R
+++ linked_extracts/00_all_extracts.R
@@ -1,4 +1,4 @@
-source("extract-data/00_setup-environment.R")
+source("extract-data/00_setup-environment.R", echo = TRUE)
 
 # Export COCIN data
 # Note you will need an API key (a pop-up will ask for it)
@@ -9,8 +9,8 @@ source("extract-data/00_setup-environment.R")
 # Note it will extract maximum once a week
 if (floor_date(today(), "week") > floor_date(ymd_hm(latest_extract_date()), "week")) {
   message("Data is from a previous week - getting new extract")
-  source("extract-data/01_get-scottish-cocin-data.R")
-  source("clean-data/clean_cocin_data.R")
+  source("extract-data/01_get-scottish-cocin-data.R", echo = TRUE)
+  source("clean-data/clean_cocin_data.R", echo = TRUE)
 }
 
 # Copy to server
@@ -34,8 +34,8 @@ if (file_exists(cocin_data_local)) {
 
 # RAPID data --------------------------------------------------------------
 # Get the RAPID data
-source("extract-data/02_read_RAPID_data.R")
-source("clean-data/clean_RAPID_data.R")
+source("extract-data/02_read_RAPID_data.R", echo = TRUE)
+source("clean-data/clean_RAPID_data.R", echo = TRUE)
 
 # Copy to server
 rapid_data_local <- path(here("data", str_glue("{date(latest_local_cocin_date())}_RAPID-cleaned-filtered.rds")))
@@ -51,11 +51,11 @@ if (file_exists(rapid_data_local)) {
 
 # SMR01 data --------------------------------------------------------------
 # Get the SMR01 data
-source("extract-data/03_extract_smr01_data.R")
+source("extract-data/03_extract_smr01_data.R", echo = TRUE)
 
 # NRS data --------------------------------------------------------------
 # Get the Deaths data
-source("extract-data/04_extract_nrs_deaths_data.R")
+source("extract-data/04_extract_nrs_deaths_data.R", echo = TRUE)
 
 # Clear database connection
 odbc::dbDisconnect(SMRA_connect)
@@ -63,8 +63,8 @@ rm(SMRA_connect)
 
 # SICSAG data --------------------------------------------------------------
 # Get the ICU data
-source("extract-data/05_read_sicsag_icu_data.R")
+source("extract-data/05_read_sicsag_icu_data.R", echo = TRUE)
 
 
 # Clean up old local data -------------------------------------------------------
-source("extract-data/99_remove-old-data.R")
+source("extract-data/99_remove-old-data.R", echo = TRUE)
